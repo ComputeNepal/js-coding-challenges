6b96fe138832e96acbfbe9d50d8da9b2dc59bd8a
ceaser008 <radiusrazzat111@gmail.com>
2023-05-21 10:30:55 +0545

    small formatting

--- solutions/ch_15_Swap_Numbers/answer.js
+++ solutions/ch_15_Swap_Numbers/answer.js
@@ -1,11 +1,11 @@
 // Declare a function swap that takes num_1 and num_2 as parameters.
-function swap(num_1,num_2){
+function swap(num_1, num_2) {
   // At first print the numbers before swaping
-    console.log("Before swapping:\n num_1 = "+num_1+"\n num_2 = "+num_2)
+  console.log("Before swapping:\n num_1 = " + num_1 + "\n num_2 = " + num_2);
   // Declare a variable temp and assigns the value of num_1 and assigns num_2 in num_1 and temp in num_2
   let temp = num_1;
-      num_1 = num_2;
-      num_2 = temp
+  num_1 = num_2;
+  num_2 = temp;
   // Check the result by printing the output
-    console.log("After swapping:\n num_1 = "+num_1+"\n num_2 = "+num_2)
+  console.log("After swapping:\n num_1 = " + num_1 + "\n num_2 = " + num_2);
 }
